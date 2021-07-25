088cb990f9639b6f649132f1738aae96c20c742d
alejo-cuello <alejo@diproach.com>
2021-07-25 12:03:02 -0300

    #7 Deleting .spec.ts

--- frontend/src/app/models/article-supplier/article-supplier.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ArticleSupplier } from './article-supplier';
-
-describe('ArticleSupplier', () => {
-  it('should create an instance', () => {
-    expect(new ArticleSupplier()).toBeTruthy();
-  });
-});
--- frontend/src/app/models/supplier/Supplier.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Supplier } from './Supplier';
-
-describe('Supplier', () => {
-  it('should create an instance', () => {
-    expect(new Supplier()).toBeTruthy();
-  });
-});
